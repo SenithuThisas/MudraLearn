ad3075a7cecaff515cd9f2dc1d9a3837b83d3705
SenithuThisas <st20306044@outlook.cardiffmet.ac.uk>
2026-06-26 11:51:02 +0530

    feat: allow CORS requests from 127.0.0.1 for frontend development

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,7 @@ app = FastAPI(title='MudraLearn API')
 # Allow React frontend to call this API
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['http://localhost:5173'],
+    allow_origins=['http://localhost:5173', 'http://127.0.0.1:5173'],
     allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*'],
